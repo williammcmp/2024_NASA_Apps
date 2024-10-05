67efde8cad4c0455af19d03629b987491da93fc7
William McMahon-Puce <william.mcm.p@gmail.com>
2024-10-05 17:32:59 +1000

    implemented double diff metho of analysis

--- src/signal_processing.py
+++ src/signal_processing.py
@@ -12,6 +12,7 @@ import pandas as pd
 
 from scipy.signal import butter, filtfilt
 
+from src.utilis import get_timed_window
 
 def highpass_filter(
     data_series: pd.Series, cutoff: float, fs: float, order: int = 5
@@ -74,3 +75,70 @@ def lowpass_filter(
     # Apply the filter
     filtered_data = filtfilt(b, a, data_series)
     return filtered_data
+
+def find_seismic_event(signal_df : pd.DataFrame, target_field: str = 'velocity(m/s)', sampleFreq : float = 6.625, time_field : str = 'time_rel(sec)', showPlot: bool = False, plotPath: str = None, expected_start : float = None) -> pd.DataFrame:
+
+    # Validate the the target field in present in the dataFrame
+    if target_field in signal_df.columns:
+        # Get the signal data series from the dataFrame
+        signal = signal_df[target_field]
+    else:
+        print(f"Warning: {target_field} is NOT a valid filed in {signal_df.columns}")
+
+    # run the band pass filter on the signal
+    high_passed_signal = highpass_filter(signal, 0.8, sampleFreq)
+    filtered_signal = lowpass_filter(high_passed_signal, 0.6, sampleFreq)
+
+    # Compute the double difference to find the maxiumin rate of change of the seismic event --> assuming this to be the peak of the seismic wave
+    filtered_signal_diff = np.diff(np.diff(filtered_signal))
+
+    # Get index of the max signal rate of change
+    peak_index = np.argmax(filtered_signal_diff)
+
+    # Get start/end  time of window
+    start_time = signal_df[time_field].iloc[peak_index] -500 # offset to capture the front of the wave
+    end_time = signal_df[time_field].iloc[peak_index] + 5000 # offset of caputer end of the wave
+
+
+    seismic_event = get_timed_window(signal_df, start_time, end_time)
+
+    # Plotting the the outcome of the Seismic wave analysis
+    if showPlot:
+        import matplotlib.pyplot as plt
+        fig, ax = plt.subplots(3, figsize=(8, 6), sharex=False, sharey=False)
+
+        ax[0].plot(signal_df[time_field], signal_df[target_field], label="Raw - Signal")
+        ax[0].set_ylabel(target_field)
+        ax[0].set_xlabel('Time (s)')
+        ax[0].axvline(x=start_time, c='red', label='Guess of start Time')
+        ax[0].set_title('Raw Signal')
+        ax[0].legend()
+
+        ax[1].plot(signal_df[time_field], filtered_signal, label="Bandpass filtered Signal")
+        ax[1].set_ylabel(target_field)
+        ax[1].set_xlabel('Time (s)')
+        ax[1].axvline(x=start_time, c='red', label='Guess of start Time')
+        ax[1].set_title('Filtered Signal')
+        ax[1].legend()
+
+        ax[2].plot(seismic_event[time_field], seismic_event[target_field], label="Raw - Seismic Event")
+        ax[2].set_ylabel(target_field)
+        ax[2].set_xlabel('Time (s)')
+        ax[2].axvline(x=start_time, c='red', label='Guess of start Time')
+        ax[2].set_title('Seismic Event')
+        ax[2].legend()
+
+        if expected_start is not None:
+            ax[0].axvline(x=expected_start, c='green', label='Expected Start - Traning Data')
+            ax[0].legend()
+            ax[1].axvline(x=expected_start, c='green')
+            ax[2].axvline(x=expected_start, c='green')
+
+        fig.tight_layout()
+        # fig.show()
+
+        # Option to save the plot to an output folder
+        if plotPath is not None:
+            fig.savefig(plotPath, dpi=300)
+
+    return seismic_event, start_time
